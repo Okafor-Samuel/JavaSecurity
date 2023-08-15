e0b3685da05a5fc4b7c09f3aa227257e29aae4ec
decagon <samuel.okafor@decagon.dev>
2023-08-15 12:16:53 +0100

    update

---
+++ src/main/java/com/example/backendsercurity/Appuser/AppUsers.java
@@ -0,0 +1,85 @@
+package com.example.backendsercurity.Appuser;
+
+import com.example.backendsercurity.Enums.AppUsersRole;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.Collections;
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@EqualsAndHashCode
+@Entity
+public class AppUsers implements UserDetails {
+
+    @Id
+   @SequenceGenerator(
+           name = "student_sequence",
+           sequenceName = "student_sequence",
+           allocationSize = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "student_sequence")
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String userName;
+    private String email;
+    private String password;
+    @Enumerated(EnumType.STRING)
+    private AppUsersRole appUserRole;
+    private boolean locked;
+    private boolean enabled;
+
+    public AppUsers(String firstName, String lastName, String userName, String email, String password, AppUsersRole appUserRole, boolean locked, boolean enabled) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+        this.appUserRole = appUserRole;
+        this.locked = locked;
+        this.enabled = enabled;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority(appUserRole.name());
+        return Collections.singletonList(authority);
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public String getUsername() {
+        return userName;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !locked;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return enabled;
+    }
+}
